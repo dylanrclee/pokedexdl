9e9333d3e15fc1dcb8ec864f956c7418b8d31f5a
LeeD108 <dylanrclee@gmail.com>
2026-08-25 18:48:27 -0400

    added scanner input and reply log

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Print("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		scan_text := scanner.Text()
+		ct := cleanInput(scan_text)
+		fmt.Printf("Your command was: %s\n", ct[0])
+	}
 }
 
 func cleanInput(text string) []string {
