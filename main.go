c4b6945f7d349fbc9207b6f3dcbd6846aa192095
LeeD108 <dylanrclee@gmail.com>
2026-08-26 11:33:12 -0400

    command struct and command mapping, added exit and help commands for terminal users

--- main.go
+++ main.go
@@ -8,14 +8,53 @@ import (
 )
 
 func main() {
+
+	type cliCommand struct {
+		name        string
+		description string
+		callback    func() error
+	}
+
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+
+		"help": {
+			name:        "help",
+			description: "explains pokedex and what commands can be used",
+			callback:    commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex >")
 		scanner.Scan()
 		scan_text := scanner.Text()
 		ct := cleanInput(scan_text)
-		fmt.Printf("Your command was: %s\n", ct[0])
+		val, ok := commands[ct[0]]
+		if ok {
+			val.callback()
+		} else {
+			fmt.Print("Unknown command")
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return fmt.Errorf("No error, exit successful")
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n")
+	fmt.Print("help: Displays a help message\n")
+	fmt.Print("exit: Exit the Pokedex\n")
+	return fmt.Errorf("No error, help successful")
 }
 
 func cleanInput(text string) []string {
