dd4ae88439db070e47afa9ae069739f49a17d16f
LeeD108 <dylanrclee@gmail.com>
2026-08-28 11:57:47 -0400

    added pokeapi.go to request and unmarshal JSON data from pokeapi; added map and mapb commands to page through location data

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ type cliCommand struct {
 
 type config struct {
 	command_list map[string]cliCommand
+	Next         string
+	Previous     string
 }
 
 func main() {
@@ -31,9 +33,22 @@ func main() {
 			description: "explains pokedex and what commands can be used",
 			callback:    commandHelp,
 		},
+
+		"map": {
+			name:        "map",
+			description: "displays next 20 locations of the pokemon world",
+			callback:    commandMap,
+		},
+
+		"mapb": {
+			name:        "mapb",
+			description: "displays previous 20 location of the pokemon world",
+			callback:    commandMapb,
+		},
 	}
 
-	comm_reg := &config{commands}
+	comm_reg := &config{}
+	comm_reg.command_list = commands
 	REPLloop((comm_reg))
 }
 
@@ -57,16 +72,54 @@ func REPLloop(comms *config) {
 	}
 }
 
-func commandExit(*config) error {
+func commandExit(cur_config *config) error {
 	fmt.Print("Closing the Pokedex... Goodbye!\n\n")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp(*config) error {
+func commandHelp(cur_config *config) error {
 	fmt.Print("Welcome to the Pokedex!\nUsage:\n")
 	fmt.Print("help: Displays a help message\n")
+	fmt.Print("map: Displays the first or next 20 locations\n")
 	fmt.Print("exit: Exit the Pokedex\n\n")
+	return nil
+}
+
+func commandMap(cur_config *config) error {
+	var call_url string
+	if cur_config.Next == "" {
+		call_url = "https://pokeapi.co/api/v2/location-area/"
+	} else {
+		call_url = cur_config.Next
+	}
+	var city_struct pokestruct
+	pokeapi(call_url, &city_struct)
+	for _, val := range city_struct.Results {
+		fmt.Print("\n", val.Name)
+	}
+	fmt.Print("\n\n")
+	cur_config.Previous = city_struct.Previous
+	cur_config.Next = city_struct.Next
+	return nil
+}
+
+func commandMapb(cur_config *config) error {
+	var call_url string
+	if cur_config.Previous == "" || cur_config.Previous == "null" {
+		fmt.Print("you're on the first page\n\n")
+		return nil
+	} else {
+		call_url = cur_config.Previous
+	}
+	var city_struct pokestruct
+	pokeapi(call_url, &city_struct)
+	for _, val := range city_struct.Results {
+		fmt.Print("\n", val.Name)
+	}
+	fmt.Print("\n\n")
+	cur_config.Previous = city_struct.Previous
+	cur_config.Next = city_struct.Next
 	return nil
 }
 
